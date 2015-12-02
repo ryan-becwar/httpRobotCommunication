db82623038c5d6a8b5ca7e23a08df4405f847d06
Ryan Becwar <rbecwar1@gmail.com>
2015-12-01 19:38:03 -0500

    Wrote code for basic proxy setup and reading in arguments

---
+++ proxy/proxy.h
@@ -0,0 +1,17 @@
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), bind(), and connect() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+#include <errno.h>
+#include <sys/types.h>
+#include <string.h>     /* for memset() */
+#include <netdb.h>      /* for getHostByName() */
+#include <stdbool.h>
+
+
+void DieWithError(char *errorMessage);  /* Error handling function */
+
+#define ECHOMAX 100
+#define RCVBUFSIZE 32   /* Size of receive buffer */
