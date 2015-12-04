f9b9f766d7bd4fb27b8f8a1bc59dd5516c4ae120
Ryan Becwar <rbecwar1@gmail.com>
2015-12-03 22:47:08 -0500

    Fleshing out proxy server and defines

--- proxy/proxy.c
+++ proxy/proxy.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]){
 
 	/*
-	===========UDP SERVER SETUP==============
+	===========UDP SERVER VARS==============
 	*/
 	int udpSock;                        /* Socket */
 	struct sockaddr_in udpServAddr; /* Local address */
@@ -12,11 +12,13 @@ int main(int argc, char *argv[]){
 	char udpBuffer[ECHOMAX];        /* Buffer for echo string */
 	unsigned short udpServPort = 8080;     /* Server port */
 	int recvMsgSize;                 /* Size of received message */
+	int password = 0;
+	char packetBuffer[300];
 
 
 
 	/*
-	===========TCP CLIENT SETUP =============
+	===========TCP CLIENT VARS =============
 	*/
 	int tcpSock;                        /* Socket descriptor */
 	struct sockaddr_in robotAddr; /* Echo server address */
@@ -54,5 +56,60 @@ int main(int argc, char *argv[]){
 	//Test print out of arguments
 	fprintf(stderr, "host: %s  robotId: %s robotNumber: %i udpServPort: %i\n",
 		host, robotId, robotNumber, udpServPort);
+
+	/*
+	===========UDP SERVER SETUP===============
+	*/
+
+	/* Create socket for sending/receiving datagrams */
+	if ((udpSock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+		DieWithError("socket() failed");
+
+	/* Construct local address structure */
+	memset(&udpServAddr, 0, sizeof(udpServAddr));   /* Zero out structure */
+	udpServAddr.sin_family = AF_INET;                /* Internet address family */
+	udpServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+	udpServAddr.sin_port = htons(udpServPort);      /* Local port */
+
+	/* Bind to the local address */
+	printf("UDPEchoServer: About to bind to port %d\n", udpServPort);    
+	if (bind(udpSock, (struct sockaddr *) &udpServAddr, sizeof(udpServAddr)) < 0)
+		DieWithError("bind() failed");
+
+	for (;;) /* Run forever */
+	{
+		/* Set the size of the in-out parameter */
+		udpCliAddrLen = sizeof(udpClntAddr);
+
+		/* Block until receive message from a client */
+		if ((recvMsgSize = recvfrom(udpSock, udpBuffer, ECHOMAX, 0,
+			(struct sockaddr *) &udpClntAddr, &udpCliAddrLen)) < 0)
+			DieWithError("recvfrom() failed");
+
+		printf("Handling client %s\n", inet_ntoa(udpClntAddr.sin_addr));
+
+		/* Send received datagram back to the client */
+//		if (sendto(udpSock, udpBuffer, recvMsgSize, 0, 
+//			(struct sockaddr *) &udpClntAddr, sizeof(udpClntAddr)) != recvMsgSize)
+//			DieWithError("sendto() sent a different number of bytes than expected");
+
+		/* Handle session with the new client*/
+		//Send password to client
+		if(sendto(udpSock, &password, sizeof(password), 0,
+			(struct sockaddr *) &udpClntAddr, sizeof(udpClntAddr)) != 4)
+			DieWithError("Sent wrong number of password bytes");
+		
+		bool exit = false;
+		while(!exit){
+			if((recvMsgSize = recvfrom(udpSock, packetBuffer, 300, 0,
+				(struct sockaddr *) &udpClntAddr, &udpCliAddrLen)) < 0)
+				DieWithError("Recieving packet failed");
+
+			printf("Request: %s", packetBuffer);
+		}
+			
+	}
+	/* NOT REACHED */	
+	
 	return 0;
 }
--- proxy/proxy.h
+++ proxy/proxy.h
@@ -15,3 +15,24 @@ void DieWithError(char *errorMessage);  /* Error handling function */
 
 #define ECHOMAX 100
 #define RCVBUFSIZE 32   /* Size of receive buffer */
+
+#define IDENTIFIER_LOC 0 
+#define PASSWORD_LOC 4
+#define CLIENT_REQUEST_LOC 8 
+#define REQUEST_DATA_LOC 12
+#define BYTE_OFFSET_LOC 16
+#define TOTAL_SIZE_LOC 20
+#define PAYLOAD_SIZE_LOC 24
+#define PAYLOAD_LOC 28
+
+#define CONNECT	0
+#define IMAGE	2
+#define GPS	4
+#define DGPS	8
+#define LASERS	16
+#define MOVE	32
+#define TURN	64
+#define STOP	128
+#define QUIT	255
+#define ERR_REQUEST	256
+#define ERR_ROBOT	512
