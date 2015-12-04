55a753d30ad6317cfdddc61f41399c94ebf52c08
Dwhite521 <devinwhite0521@gmail.com>
2015-12-04 09:01:56 -0500

    More work on communicate

--- proxy/communicate.c
+++ proxy/communicate.c
@@ -1,11 +1,3 @@
-//
-//  temp.c
-//  
-//
-//  Created by Devin White on 12/3/15.
-//
-//
-
 #include "proxy.h"
 
 void communicate(int requestType, char* host, int robot_number, char* robot_id, int data){
@@ -14,7 +6,7 @@ void communicate(int requestType, char* host, int robot_number, char* robot_id, 
     int port = -1;     /* Echo server port */
     char filename[30];             /* The save name for output */
     char fileRequest[100];
-    char removeHeader[1024];
+    char payload[1024];
     char* doc = NULL;
     char echoString[300];                /* String to send to echo server */
     char echoBuffer[1024];     /* Buffer for echo string */
@@ -22,7 +14,6 @@ void communicate(int requestType, char* host, int robot_number, char* robot_id, 
                      and total bytes read */
     char request[60];
     
-    requestType = 8;
     switch(requestType) {
         //Image request
         case 2:
@@ -34,37 +25,37 @@ void communicate(int requestType, char* host, int robot_number, char* robot_id, 
         case 4:
             port = 8082;
             sprintf(request, "/state?id=%s", robot_id);
-            sprintf(filename,"output.txt");
+            sprintf(filename,"output4.txt");
             break;
         //DGPS request
         case 8:
             port = 8084;
             sprintf(request, "/state?id=%s", robot_id);
-            sprintf(filename,"output.txt");
+            sprintf(filename,"output8.txt");
             break;
         //lasers request
         case 16:
             port = 8083;
             sprintf(request, "/state?id=%s", robot_id);
-            sprintf(filename,"output.txt");
+            sprintf(filename,"output16.txt");
             break;
         //move request
         case 32:
             port = 8082;
             sprintf(request, "/state?id=%s&lx=%d", robot_id, data);
-            sprintf(filename,"output.txt");
+            sprintf(filename,"output32.txt");
             break;
         //turn request
         case 64:
             port = 8082;
             sprintf(request, "/state?id=%s&lz=%d", robot_id, data);
-            sprintf(filename,"output.txt");
+            sprintf(filename,"output64.txt");
             break;
         //stop request
         case 128:
             port = 8082;
             sprintf(request, "/state?id=%s&lx=0", robot_id);
-            sprintf(filename,"output.txt");
+            sprintf(filename,"output128.txt");
             break;
         default:
             break;
@@ -72,12 +63,6 @@ void communicate(int requestType, char* host, int robot_number, char* robot_id, 
     if(access(filename, F_OK) != -1){
         remove(filename);
     }
-    //    if(filename[0] == '\0'){
-    //        strcpy(filename, "stdout");
-    //    }
-    printf("Port: %d, Saving output to: %s, Host: %s\n"
-           "Getting file: %s\n", port, filename, host, request);
-    
     
     /* Create a reliable, stream socket using TCP */
     if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
@@ -108,32 +93,74 @@ void communicate(int requestType, char* host, int robot_number, char* robot_id, 
     }
     memset(echoString, 0, 300);
     printf("\nRECEIVED THIS FROM THE SERVER: \n");
-    FILE *fp = fopen("output.txt", "wb");
+    FILE *fp = fopen(filename, "w");
     
     unsigned int totalBytes=0;
     char payloadSize[30];
     bytesRec = recv(sock, echoBuffer, 1024, 0);
-    //    memset(removeHeader, 0, sizeof(removeHeader));
-    sprintf(removeHeader, "%s", strtok(echoBuffer, "\n"));
-    sprintf(removeHeader, "%s", strtok(NULL, " "));
-    sprintf(payloadSize, "%s", strtok(NULL, "\n"));
-    sprintf(removeHeader, "%s", strtok(NULL, "\n"));
-    sprintf(removeHeader, "%s", strtok(NULL, "\n"));
-    sprintf(removeHeader, "%s", strtok(NULL, "\n"));
-    sprintf(removeHeader, "%s", strtok(NULL, "\n"));
-    sprintf(removeHeader, "%s", strtok(NULL, "\n"));
-    printf("%s\n\n", payloadSize);
+    //    memset(payload, 0, sizeof(payload));
+    if(requestType==2){
+        printf("---%s\n", echoBuffer);
+        sprintf(payload, "%s", strtok(echoBuffer, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+
+
+        printf("This: %s is payload\n", payload);
+        if(strlen(payload)>26){
+            fprintf(fp,"%s", payload);
+            totalBytes+=strlen(payload);
+        }
+        printf("%d bytes written", totalBytes);
+        //memset(payload, 0, sizeof(payload));
+        memset(echoBuffer, 0, bytesRec);
+        
+        if(bytesRec<0) DieWithError("recv() failed or connection closed prematurely");
+        
+        while ((bytesRec = recv(sock, echoBuffer, 1024, 0)) > 0){
+            totalBytes+=bytesRec;
+            fprintf(fp,"%s", echoBuffer);
+            //        printf("%s", echoBuffer);
+            memset(echoBuffer, 0, bytesRec);
+            
+            if(bytesRec<0) DieWithError("recv() failed or connection closed prematurely");
+        }
+
+        printf("\n total bytes read %u", totalBytes);
+        fclose(fp);
+        printf("\n");
+        
+        close(sock);
+        
+    }else{
+//        printf("%s\n----", echoBuffer);
+        sprintf(payload, "%s", strtok(echoBuffer, "\n"));
+        sprintf(payload, "%s", strtok(NULL, " "));
+        sprintf(payloadSize, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\n"));
+        sprintf(payload, "%s", strtok(NULL, "\r\n"));
     
+    printf("This number %s\n\n", payloadSize);
     
-    totalBytes+=sizeof(removeHeader);
-    fprintf(fp,"%s", removeHeader);
-    printf("%s", removeHeader);
-    memset(removeHeader, 0, sizeof(removeHeader));
+        if(strlen(payload)>26){
+            fprintf(fp,"%s", payload);
+            totalBytes+=strlen(payload);
+        }
+    printf("%d bytes written", totalBytes);
+    //memset(payload, 0, sizeof(payload));
     memset(echoBuffer, 0, bytesRec);
     
     if(bytesRec<0) DieWithError("recv() failed or connection closed prematurely");
     
-    while ((bytesRec = recv(sock, echoBuffer, 1024, 0)) > 0){
+    while (totalBytes != atoi(payloadSize) && (bytesRec = recv(sock, echoBuffer, 1024, 0)) > 0){
         totalBytes+=bytesRec;
         fprintf(fp,"%s", echoBuffer);
         //        printf("%s", echoBuffer);
@@ -141,10 +168,13 @@ void communicate(int requestType, char* host, int robot_number, char* robot_id, 
         
         if(bytesRec<0) DieWithError("recv() failed or connection closed prematurely");
     }
-    //fprintf(fp,"%s", totalBuffer);
+//    fseek(fp, 0L, SEEK_END);
+//    totalBytes = ftell(fp);
+//    fseek(fp, 0L, SEEK_SET);
+    printf("\n total bytes read %u", totalBytes);
     fclose(fp);
     printf("\n");
     
     close(sock);
-    
+    }
 }
